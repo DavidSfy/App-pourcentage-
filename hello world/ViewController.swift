067cd542c374b4ae0115e9bc511c5096e63b78cc
DavidSfy <dserfaty22@gmail.com>
2017-09-13 23:35:48 +0200

    fonction

--- hello world/ViewController.swift
+++ hello world/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipAmountTextfield: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var pourcentLabel: UITextField!
+    @IBOutlet weak var restePayer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello world")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func computeTip(_ sender: UIButton) {
+        
+        tipLabel.text = String( Float(tipAmountTextfield.text!)! * Float(pourcentLabel.text!)! * 0.01)
+    
+        restePayer.text = String(Float(tipAmountTextfield.text!)! - Float(tipLabel.text!)!)
 }
-
+}
